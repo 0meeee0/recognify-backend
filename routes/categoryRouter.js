27687271616fd660de9d1fbc05e3c89272f9d0c0
0meeee0 <mehdi74.id@gmail.com>
2025-04-29 22:21:56 +0100

    minor: adjusted authorization for categories and courses

--- routes/categoryRouter.js
+++ routes/categoryRouter.js
@@ -3,11 +3,12 @@ const router = express.Router();
 const categoryController = require('../controller/categoryController');
 const {authenticate} = require('../middleware/authMiddleware')
 
+router.get('/', categoryController.getCategories);
+router.get('/:id', categoryController.getCategoryById);
+
 router.use(authenticate)
 
 router.post('/', categoryController.createCategory);
-router.get('/', categoryController.getCategories);
-router.get('/:id', categoryController.getCategoryById);
 router.put('/:id', categoryController.updateCategory);
 router.delete('/:id', categoryController.deleteCategory);
 
--- routes/courseRouter.js
+++ routes/courseRouter.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const courseController = require('../controller/courseController');
 const {authenticate} = require('../middleware/authMiddleware')
 
-router.use(authenticate)
-
-router.post('/', courseController.createCourse);
 router.get('/', courseController.getCourses);
 router.get('/:id', courseController.getCourseById);
+
+router.use(authenticate)
+router.post('/', courseController.createCourse);
 router.put('/:id', courseController.updateCourse);
 router.delete('/:id', courseController.deleteCourse);
 
